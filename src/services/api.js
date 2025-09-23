9096bafd869b0233c2a75608b163b93d6fbbb737
MURALA SAI GANESH <163727585+MSGanesh23@users.noreply.github.com>
2025-09-23 11:52:50 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 
-const API_BASE_URL = 'http://localhost:4570/api/videos'; // Adjust the base URL as needed
+const API_BASE_URL = 'http://localhost:8086/api/videos'; // Adjust the base URL as needed
 
 // Function to fetch all videos
 export const fetchVideos = async () => {
