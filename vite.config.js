1af7d23e5b7b03296bd11c8fde1e1075630aa83f
MURALA SAI GANESH <163727585+MSGanesh23@users.noreply.github.com>
2025-09-24 10:18:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-
-const basePath = process.env.VITE_BASE || '/'
-
+// 👇 Hardcode the base path to match your Tomcat context path
 export default defineConfig({
-  base: basePath,
+  base: '/frontapp1/', // ✅ This must match your WAR context (frontapp1)
   plugins: [react()]
 })
