d931a71adcc2f5c8224ea18cb2b4a342af7f3339
MURALA SAI GANESH <163727585+MSGanesh23@users.noreply.github.com>
2025-09-25 07:48:14 +0530

    Update Signin.jsx

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -15,7 +15,7 @@ const Signin = () => {
         }
 
         try {
-            const response = await fetch("http://localhost:8080/api/login", {
+            const response = await fetch("http://localhost:8030/api/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, password }),
